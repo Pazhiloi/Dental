8276eaf7df2328eef1e875414c46929decdb79d9
Pazhiloi <martinsen06@gmail.com>
2021-04-15 22:25:45 +0300

    adaptive ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,14 @@
 $(function () {
+   $('.clients__input--one').on('click', function () {
+  $('.clients__item').toggleClass('clients__item--one')
+});
+   $('.clients__input--two').on('click', function () {
+  $('.clients__item').toggleClass('clients__item--two')
+});
+   $('.clients__input--three').on('click', function () {
+  $('.clients__item').toggleClass('clients__item--three')
+});
+ $('.menu__btn').on('click', function () {
+  $('.menu__list').toggleClass('menu__list--active')
+});
 });
